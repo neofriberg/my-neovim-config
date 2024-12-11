e29338dbdf3cc19e7ce36b7d551516c191eefa18
= <neofriberg@live.se>
2024-12-11 17:31:58 +0100

    undotree and fugitive config

--- lua/neofri/packer.lua
+++ lua/neofri/packer.lua
@@ -20,4 +20,11 @@ return require('packer').startup(function(use)
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
 	use('theprimeagen/harpoon')
+
+	use('mbbill/undotree')
+
+	use('tpope/vim-fugitive')
+
+
+
 end)
