1e021507009993629a003fd56ed10d85db403cee
= <neofriberg@live.se>
2024-12-11 17:16:16 +0100

    colorscheme + treesitter config

--- lua/neofri/packer.lua
+++ lua/neofri/packer.lua
@@ -8,4 +8,14 @@ return require('packer').startup(function(use)
 		'nvim-telescope/telescope.nvim', tag = '0.1.8',
 		requires = {{'nvim-lua/plenary.nvim'}}
 	}
+	use ({
+
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
+
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 end)
